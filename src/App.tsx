0ba2cde5604151768b6992b2b37b0353214059a2
OlAndrey <oleynik.andrey01@gmail.com>
2023-07-11 23:53:31 +0300

    feat(Canvas): Canvas component created and styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./styles/app.css"
+import Canvas from './components/Canvas'
 import ToolBar from './components/Toolbar'
 
 function App() {
   return (
     <div className="app">
       <ToolBar />
+      <Canvas />
     </div>
   )
 }
